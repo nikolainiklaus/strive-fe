fb0c8601a0243b6f400e0f5271c92187fb501d8c
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-03-06 16:30:41 +0100

    env

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,8 +2,26 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
+import { useState, useEffect } from "react";
 
 const BlogList = (props) => {
+  const [posts, setPosts] = useState([]);
+  const apiURL = process.env.REACT_APP_URL
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch("http://localhost:3001/blogposts");
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+
   return (
     <Row>
       {posts.map((post) => (
